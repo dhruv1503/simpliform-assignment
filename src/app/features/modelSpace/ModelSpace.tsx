0d418f21bba4f6c22b3ff149c08dc079c4f84746
Dhruv Aggarwal <dhruvaggarwal1503@gmail.com>
2024-08-31 13:32:17 +0530

    added:model space layout

--- src/app/features/modelSpace/ModelSpace.tsx
+++ src/app/features/modelSpace/ModelSpace.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { Flex } from "@chakra-ui/react";
+import { Divider, Flex, Grid, GridItem } from "@chakra-ui/react";
 import { SpaceLayout } from "../../../layouts/space-layout";
 import { ModelSpaceHeader } from "./components/ModelSpaceHeader";
 import { TModelSpaceHeaderProps } from "../../../types/props";
@@ -19,18 +19,37 @@ export const ModelSpace = () => {
   //api call will happen here
   return (
     <SpaceLayout>
-      <Flex as="main" bgColor="black" display="flex" justifyContent="center">
-        <Flex
+      <Flex as="main" bgColor="black" display="flex" justifyContent="center" background="#000000" minH={"calc(100vh - 54px)"}  maxH={"calc(100vh - 54px - 8px)"}>
+        <Grid
           height="100%"
           width="100%"
           bg="#202123"
           color="#FFF"
           maxW={"calc(100vw - 16px)"}
-          justifyItems="center"
+          maxH="100%"
+          minH={"calc(100vh - 54px - 8px)"}
           borderRadius="8px"
-        >
+          gridTemplateColumns={["repeat(12, 10fr)"]}
+          gridTemplateRows={["1fr 6fr 1fr"]}
+        >  
+        <GridItem gridColumn={"1 / span 9"} height="100%" width="100%">
           <ModelSpaceHeader name={exampleData.name} avatar={exampleData.avatar} description={exampleData.description} />
-        </Flex>
+         
+          </GridItem>
+          
+          <GridItem  gridColumnStart={10}colSpan={3} rowSpan={3} display="flex">
+            {/* Measurements || Specs */}
+            <Divider orientation="vertical" borderColor="black"/>
+          </GridItem>
+          <GridItem  colSpan={9} display="flex">
+            {/* Output area */}
+            <Divider borderColor="black"/>
+          </GridItem>
+          <GridItem colSpan={9} display="flex">
+            {/* Input Area  */}
+          </GridItem>
+        </Grid>
+       
       </Flex>
     </SpaceLayout>
   );
